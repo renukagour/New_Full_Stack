0adf4ec3f0f09c14ae2a17a5bcf0271a6b8654ee
Renuka Gour <2110041@ritindia.edu>
2026-09-24 12:59:55 +0530

    added routers

--- Projects/YoutubeDemo/src/routes/user.route.js
+++ Projects/YoutubeDemo/src/routes/user.route.js
@@ -38,7 +38,7 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
-router.route("/get-user").get(verifyJWT, getCurrentUser);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 router
   .route("/avatar")
