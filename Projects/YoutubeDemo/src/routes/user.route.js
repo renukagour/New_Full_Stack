63db8783dc3d2239e96b2bead4882aea801d16b6
Renuka Gour <2110041@ritindia.edu>
2026-09-24 12:43:06 +0530

    added rouers

--- Projects/YoutubeDemo/src/routes/user.route.js
+++ Projects/YoutubeDemo/src/routes/user.route.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { getCurrentUser, logOutUser, loginUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  logOutUser,
+  loginUser,
+  refreshAccessToken,
+  registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -8,23 +20,35 @@ const router = Router();
 router.route("/register").post(
   upload.fields([
     {
-    name:"avatar",
-    maxCount:1
-  },
-  {
-    name:"coverImage",
-    maxCount:1
-  }
-]), //accept multiple files
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]), //accept multiple files
   registerUser
 );
 
-router.route('/login').post(loginUser);
+router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post(verifyJWT,logOutUser);
+router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/get-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
-router.route('/get-user').get(getCurrentUser)
+//taking data from params
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
